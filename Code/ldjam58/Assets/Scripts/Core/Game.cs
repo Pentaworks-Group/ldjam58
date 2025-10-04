54181b8c260d6858220d128d74d9af86b932dd35
XpArKeR <xparker@gmx.net>
2025-10-04 09:02:00 +0200

    remove showTouchPads code.

--- Code/ldjam58/Assets/Scripts/Core/Game.cs
+++ Code/ldjam58/Assets/Scripts/Core/Game.cs
@@ -8,25 +8,17 @@ namespace Assets.Scripts.Core
         protected override GameState InitializeGameState()
         {
             return new GameState()
-            { 
+            {
             };
         }
 
         protected override PlayerOptions InitializePlayerOptions()
         {
-            var showTouchPads = false;
-
-            if (SystemInfo.deviceType == DeviceType.Handheld)
-            {
-                showTouchPads = true;
-            }
-
             return new PlayerOptions()
             {
                 EffectsVolume = 0.9f,
                 AmbienceVolume = 0.1f,
                 BackgroundVolume = 0.3f,
-                //ShowTouchPads = showTouchPads
             };
         }
 
