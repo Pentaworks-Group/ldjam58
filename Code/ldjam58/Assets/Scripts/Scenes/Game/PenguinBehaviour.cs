b23ee98d02d9a04ff353241262bca6164946428a
XpArKeR <xparker@gmx.net>
2025-10-04 17:17:14 +0200

    Template

---
+++ Code/ldjam58/Assets/Scripts/Scenes/Game/PenguinBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using Assets.Scripts.Core.Model;
+
+using UnityEngine;
+
+namespace Assets.Scripts.Scenes.Game
+{
+    public class PenguinBehaviour : MonoBehaviour
+    {
+        private Penguin penguin;
+
+        public void Init(Penguin penguin)
+        {
+            this.penguin = penguin;
+        }
+    }
+}
