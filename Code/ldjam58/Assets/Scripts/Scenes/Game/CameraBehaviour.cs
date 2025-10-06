ab8afe3b6ac9d529052d6ee4fec72e3b396e9c86
Rentem <salasam@gmx.net>
2025-10-06 13:17:25 +0200

    even more testing

--- Code/ldjam58/Assets/Scripts/Scenes/Game/CameraBehaviour.cs
+++ Code/ldjam58/Assets/Scripts/Scenes/Game/CameraBehaviour.cs
@@ -8,6 +8,61 @@ namespace Assets.Scripts.Scenes.Game
     public class CameraBehaviour : MonoBehaviour
     {
         private float prevMagnitude = 0;
+        private int touchCount = 0;
+
+        private void Start()
+        {
+            // pinch gesture
+            var touch0contact = new InputAction
+            (
+                type: InputActionType.Button,
+                binding: "<Touchscreen>/touch0/press"
+            );
+            touch0contact.Enable();
+            var touch1contact = new InputAction
+            (
+                type: InputActionType.Button,
+                binding: "<Touchscreen>/touch1/press"
+            );
+            touch1contact.Enable();
+
+            touch0contact.performed += _ => touchCount++;
+            touch1contact.performed += _ => touchCount++;
+            touch0contact.canceled += _ =>
+            {
+                touchCount--;
+                prevMagnitude = 0;
+            };
+            touch1contact.canceled += _ =>
+            {
+                touchCount--;
+                prevMagnitude = 0;
+            };
+
+            var touch0pos = new InputAction
+            (
+                type: InputActionType.Value,
+                binding: "<Touchscreen>/touch0/position"
+            );
+            touch0pos.Enable();
+            var touch1pos = new InputAction
+            (
+                type: InputActionType.Value,
+                binding: "<Touchscreen>/touch1/position"
+            );
+            touch1pos.Enable();
+            touch1pos.performed += _ =>
+            {
+                if (touchCount < 2)
+                    return;
+                var magnitude = (touch0pos.ReadValue<Vector2>() - touch1pos.ReadValue<Vector2>()).magnitude;
+                if (prevMagnitude == 0)
+                    prevMagnitude = magnitude;
+                var difference = magnitude - prevMagnitude;
+                prevMagnitude = magnitude;
+                MoveCam(-difference);
+            };
+        }
         private void OnEnable()
         {
             HookActions();
@@ -40,11 +95,6 @@ namespace Assets.Scripts.Scenes.Game
         private void HandleMultiTouch(InputAction.CallbackContext context)
         {
             Debug.Log("MultitouchContext" + context);
-            //var magnitude = (touch0pos.ReadValue<Vector2>() - touch1pos.ReadValue<Vector2>()).magnitude;
-            //if (prevMagnitude == 0)
-            //    prevMagnitude = magnitude;
-            //var difference = magnitude - prevMagnitude;
-            //prevMagnitude = magnitude;
         }
 
         private void HandleScroll(InputAction.CallbackContext context)
